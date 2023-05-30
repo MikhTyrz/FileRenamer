b2969bb2845d244b304c01626614cfec03571ba7
MikhTyrz <46541930+MikhTyrz@users.noreply.github.com>
2023-05-30 22:26:16 +0200

    Add files via upload

---
+++ Renamer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileRenamer
+{
+    class Renamer
+    {
+        public static void Rename()     //И здесь жопа, я пытаюсь вернуть массив, чтобы проверить 
+        {
+            string [] filesToRename = Directory.GetFiles(VistaDir.path);
+            foreach (string file in filesToRename)
+            {
+                string fe = Path.GetExtension(file);
+                char[] a = Path.GetFileNameWithoutExtension(file).ToCharArray();    //Разобрали строку на символы, выдернув из нее имя файла без расширения
+                for(int i = 0; i < a.Length; i++)
+                {
+                    if(Char.IsLetter(a[i]) == true)
+                    {
+                        continue;
+                    }
+                    else if (Char.IsNumber(a[i]) == true)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        a[i] = '_';    //Поменяли все, что требуется
+                    }
+                }
+
+                string fn = new string(a);    //Типа собрали массив обратно и дальше магия
+                string newFileName = $@"{fn}{fe}";
+                string newFileFullPath = Path.Combine(VistaDir.path, newFileName);
+                File.Move(file, newFileFullPath);
+            }
+
+            
+        }
+    }
+}
---
+++ VistaDir.cs
@@ -0,0 +1,20 @@
+﻿/*Класс открытия файла Windows Forms с использованием библиотеки Ookii. Dialogs*/
+
+using System;
+using System.Windows.Forms;
+using Ookii.Dialogs;
+
+public class VistaDir
+{
+    public static string path;
+    public static void Open()
+    {
+        path = "";
+        VistaFolderBrowserDialog of = new VistaFolderBrowserDialog();
+        DialogResult result = of.ShowDialog();
+        if (result == DialogResult.OK)
+        {
+            path = of.SelectedPath;
+        }
+    }
+}
